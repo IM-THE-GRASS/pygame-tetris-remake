880963ee060600cc557fecd20116a6b189d28168
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-06-25 16:50:20 -0600

    surfaces

--- main.py
+++ main.py
@@ -17,11 +17,13 @@ center = (window_width/2,window_height/2)
 
 
 screen = pygame.display.set_mode((window_width, window_height))
-def draw_rect(size, pos, color = (0,0,0), surface = screen):
-    rec = pygame.Rect(0,0,0,0)
-    rec.size = size
-    rec.center = pos
-    pygame.draw.rect(screen, color, rec)
+def draw_rect(size, pos, surface = screen, color = (0,0,0)):
+    rec = pygame.Surface(size)
+    topleft = (pos[0] - (size[0] / 2),pos[1] - (size[1] / 2))
+    rec.fill(color)
+    surface.blit(rec,topleft)
+    rec.fill(color)
+    return rec
 pygame.display.set_caption("pygame tetris")
 running = True
 while running:
@@ -29,13 +31,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    for i in range(width):
-        sq_width = board_width - (gap * (width + 1))
-        print(sq_width)
-        #sq = pygame.Surface()
     
     
     
     screen.fill((255, 255, 255))
+    
+    for i in range(width):
+        sq_width = board_width - (gap * (width + 1))
+        sq = draw_rect((sq_width,sq_width),(10,10), board, (255,0,255))
+        print(sq)
     board = draw_rect((board_width,board_height),center)
     pygame.display.flip()
