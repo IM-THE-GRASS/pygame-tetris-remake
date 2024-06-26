39870e1460bd928dad65cf6f536cdeb7e2d9ad67
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-06-26 13:14:51 -0600

    the grid

--- main.py
+++ main.py
@@ -5,25 +5,22 @@ pygame.init()
 board_width = 200
 board_height = 400
 
-lines = 3
-width = 2
-gap = 10
+
+columns = 5
+gap = 7
+sq_width = (board_width - (gap * (columns + 1))) / columns 
+print(sq_width)
+lines = int(board_height / (sq_width + gap))
+
 
 window_width = 400
-window_height = 500
+window_height = 600
 
 center = (window_width/2,window_height/2)
- 
+
 
 
 screen = pygame.display.set_mode((window_width, window_height))
-def draw_rect(size, pos, surface = screen, color = (0,0,0)):
-    rec = pygame.Surface(size)
-    topleft = (pos[0] - (size[0] / 2),pos[1] - (size[1] / 2))
-    rec.fill(color)
-    surface.blit(rec,topleft)
-    rec.fill(color)
-    return rec
 pygame.display.set_caption("pygame tetris")
 running = True
 while running:
@@ -36,9 +33,19 @@ while running:
     
     screen.fill((255, 255, 255))
     
-    for i in range(width):
-        sq_width = board_width - (gap * (width + 1))
-        sq = draw_rect((sq_width,sq_width),(10,10), board, (255,0,255))
-        print(sq)
-    board = draw_rect((board_width,board_height),center)
+    board = pygame.Rect(0,0,0,0)
+    board.size = (board_width,board_height)
+    board.center = center
+    
+    pygame.draw.rect(screen, (0,0,0), board)
+    for line in range(lines):
+        for i in range(columns):
+            
+            sq = pygame.Rect(0,0,0,0)
+            sq.size = (sq_width,sq_width)
+            sq.center = (10,10)
+            sq.bottomleft = (board.bottomleft[0] + gap + ((i * gap) + (i * sq_width)), board.bottomleft[1] - gap - ((line * gap) + (line * sq_width))) 
+            pygame.draw.rect(screen, (255,255,255), sq)
+    
+        
     pygame.display.flip()
