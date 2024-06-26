3d5f4b1d534c8d2560b62346c9a4691c97b0d4b6
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-06-26 17:34:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ board_width = 200
 board_height = 400
 
 
-columns = 5
-gap = 7
+columns = 10
+gap = 2
 sq_width = (board_width - (gap * (columns + 1))) / columns 
 print(sq_width)
 lines = int(board_height / (sq_width + gap))
@@ -17,14 +17,17 @@ window_width = 400
 window_height = 600
 
 center = (window_width/2,window_height/2)
-I = {(0,1),(1,1),(2,1),(3,1)}
+I = ((0,0),(1,0),(2,0),(3,0))
 class peice:
     def __init__(self,shape) -> None:
         self.shape = shape
     def draw_at(self, pos):
         for square in self.shape:
-            square = squares[square]
-            pygame.draw.rect(screen, (255,0,255), square)
+            print(square)
+            offset_x = I[0][0] + pos[0]
+            print(offset_x)
+            #square["color"] = (255,0,255)
+            #square["state"] = 0
             
 
 screen = pygame.display.set_mode((window_width, window_height))
@@ -38,27 +41,23 @@ for line in range(lines):
             
         sq = pygame.Rect(0,0,0,0)
         sq.size = (sq_width,sq_width)
-        sq.center = (10,10)
-        sq.bottomleft = (board.bottomleft[0] + gap + ((i * gap) + (i * sq_width)), board.bottomleft[1] - gap - ((line * gap) + (line * sq_width))) 
+        sq.topleft = (board.topleft[0] + gap + ((i * gap) + (i * sq_width)), board.topleft[1] + gap + ((line * gap) + (line * sq_width))) 
         
-        squares[(line, i)] = sq
-        print((line, i))
+        squares[(line, i)] = {"rect": sq, "color": (255,255,255), "state": 0}
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    
-    
-    
     screen.fill((255, 255, 255))
-    
-    
-    
     pygame.draw.rect(screen, (0,0,0), board)
     
-    for square in squares.values():
-        pygame.draw.rect(screen, (255,255,255), square)
+    
+    
     peices = peice(I)
     peices.draw_at((0,0))
+    squares[(0,1)]["color"] = (244,24,42)
+    squares[(0,5)]["color"] = (244,24,42)
+    for square in squares.values():
+        pygame.draw.rect(screen, square["color"], square["rect"])
+    
     pygame.display.flip()
