e7c321c36087edcb2416874b0fbbe555aa1cb7b6
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-06-29 10:34:37 -0600

    rotations

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ board_width = 200
 board_height = 400
 
 
-columns = 10
+columns = 13
 gap = 2
 sq_width = (board_width - (gap * (columns + 1))) / columns 
 print(sq_width)
@@ -15,16 +15,22 @@ squares = {}
 gravity_wait = 0
 window_width = 400
 window_height = 600
+start_pos = (int(columns / 2), lines - 4)
 
 center = (window_width/2,window_height/2)
-I = ((0,0),(2,0),(1,0),(8,0))
+I = {0:((0,0),(1,0),(2,0),(3,0)), 1:((0,-0),(0,-1),(0,-2),(0,-3)),2:((0,0),(-1,0),(-2,0),(-3,0)), 3:((0,0),(0,1),(0,2),(0,3))}
+T = {0:((0,0),(1,0),(0,1),(-1,0)),3:((0,0),(0,1),(1,0),(0,-1)),2:((0,0),(1,0),(0,-1),(-1,0)),1:((0,0),(0,1),(0,-1),(-1,0))}
+O = {0:((0,0),(0,1),(1,0),(1,1)),1:((0,0),(0,1),(1,0),(1,1)),2:((0,0),(0,1),(1,0),(1,1)),3:((0,0),(0,1),(1,0),(1,1))}
+
 class peice:
     def __init__(self,shape, position) -> None:
         self.pos = position
+        self.rotation = 0
         self.shape = shape
-        self.current = {}
+        self.current_positions = {}
         self.last_valid_pos = ()
         self.draw()
+        
         
         
     def draw(self):
@@ -32,7 +38,7 @@ class peice:
         
         
         
-        for square in self.shape:
+        for square in self.shape[self.rotation]:
             new_x = square[0] + self.pos[0]
             new_y = square[1] + self.pos[1]
             new_pos = (new_x,new_y)
@@ -42,22 +48,38 @@ class peice:
             except:
                 self.pos = self.last_valid_pos
                 return False
-        for square in self.current:
+        for square in self.current_positions:
             thesquare = squares[square]
             thesquare["color"] = (255,255,255)
             thesquare["state"] = 0
-        self.current.clear()
+        self.current_positions.clear()
         for square in new_current:
             position = squares[square]
             position["color"] = (255,0,255)
             position["state"] = 1
-            self.current[square] = 1
+            self.current_positions[square] = 1
             self.last_valid_pos = self.pos
-def rotate_r(peice):
-    peice.shape = ((0,0))
-    print("hi")
-def rotate_l(peice):
-    print("hi")
+    def rotate_r(self):
+        if self.rotation >= 3:
+            self.rotation = 0 
+        else:
+            self.rotation = self.rotation + 1
+        draw = self.draw()
+        if draw == False and self.rotation >= 3:
+            self.rotation = self.rotation - 1
+        
+            
+    def rotate_l(self):
+        print(self.rotation)
+        if self.rotation <= 0:
+            self.rotation = 3 
+        else:
+            self.rotation = self.rotation - 1
+        print(self.rotation)
+        draw = self.draw()
+        if draw == False and self.rotation < 0:
+            self.rotation = self.rotation + 1
+        print(self.rotation)
                
             
 
@@ -76,16 +98,16 @@ for line in range(lines):
         sq.bottomleft = (board.bottomleft[0] + gap + ((i * gap) + (i * sq_width)), board.bottomleft[1] - gap - ((line * gap) + (line * sq_width))) 
         
         squares[(i, line)] = {"rect": sq, "color": (255,255,255), "state": 0}
-peices = peice(I, (0,lines - 1))
+peices = peice(O, start_pos)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == pygame .BUTTON_RIGHT:
-                rotate_r(peices)
+                peices.rotate_r()
             if event.button == pygame.BUTTON_LEFT:
-                rotate_l(peices)
+                peices.rotate_l()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 peices.pos = (peices.pos[0] + 1, peices.pos[1])
